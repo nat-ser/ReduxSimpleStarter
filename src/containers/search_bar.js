021a61dd2e58f0f6ec7d0ed69b8bf31774327afc
nataliya seryakova <nataliya.seryakova@gmail.com>
2018-05-21 15:44:17 -0400

    search

---
+++ src/containers/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class SearchBaer extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({ term: event.target.value });
+		console.log(event.target.value)
+	}
+
+	onFormSubmit(event) {
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.onFormSubmit} className='input-group'>
+				<input  
+					placeholder='get 5 day forecast'
+					className='form-control'
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
+				<span className='input-group-btn'>
+					<button type='submit' className='btn btn-secondary'>Submit</button>
+				</span>
+			</form>
+		)
+	}
+}
